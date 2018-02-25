87e492323c12d6bb3839e3c638043838dc67d700
Dante Henrique Strutzel Saggin <dante.saggin@gmail.com>
2018-02-24 22:57:06 -0300

    Layout done - Needs fix on some constraints Capturing image and updating the size on the screen

--- CompressImage/ViewController.swift
+++ CompressImage/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        var mb:Double = 0
+        UiImageView.image =  image
+        if let imageData = UIImagePNGRepresentation(image!) {
+            let bytes = imageData.count
+            mb = Double(bytes) / (1024.0 * 1024)
+        } else if let imageData = UIImageJPEGRepresentation(image!, 1){
+            
+                let bytes = imageData.count
+                mb = Double(bytes) / (1024.0 * 1024)
+        }
+        picker.dismiss(animated: true, completion: nil)
+        imgOriginalSize.text = String(format: "%.2f Mb", mb)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var UiImageView: UIImageView!
+    @IBOutlet weak var imgCompressedSize: UILabel!
+    @IBOutlet weak var imgOriginalSize: UILabel!
+    
+    @IBAction func captureImageAction(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func compressImageAction(_ sender: UIButton) {
+        
+    }
+    
 }
 
